c9f68a8070ee39d04f91fd171c69f8c79d4f2ef9
ovarn <barsukox@gmail.com>
2023-09-29 00:37:28 +0200

    02-components

--- 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
@@ -1,23 +1,47 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    agendaItemIcon() {
+      const agendaType = this.agendaItem.type;
+      return agendaItemIcons[agendaType] || agendaItemIcons.opening;
+    },
+
+    agendaItemTitle() {
+      const agendaType = this.agendaItem.type;
+      const agendaItemTitle = this.agendaItem.title;
+      return agendaItemTitle || agendaItemDefaultTitles[agendaType];
+    },
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="\`/assets/icons/icon-\${agendaItemIcon}.svg\`" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <time :datetime="agendaItem.startsAt">{{ agendaItem.startsAt }}</time>
+          -
+        <time :datetime="agendaItem.endsAt">{{ agendaItem.endsAt }}</time>
+      </div>
+      <div class="agenda-item__col">
+        <h3 class="agenda-item__title">{{ agendaItemTitle }}</h3>
+        <p class="agenda-item__talk" v-if="agendaItem.type == 'talk'">
+          <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,63 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../../../02-components/06-MeetupView/components/MeetupView.js';
+import { fetchMeetupById } from '.././meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
 
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  data() {
+    return {
+      meetup: null,
+      isLoading: true,
+      errMsg: '',
+    }
+  },
+
+  methods: {
+    async getMeetup(id) {
+      this.isLoading = true;
+      try {
+        this.meetup = await fetchMeetupById(id);
+        this.errMsg = '';
+      } catch(e) {
+        this.errMsg = e.message;
+      }
+      this.isLoading = false;
+    },
+  },
+
+  watch: {
+    meetupId: {
+      handler(newMeetupId) {
+        this.getMeetup(newMeetupId);
+      },
+      immediate: true,
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
-
-      <UiContainer>
+      <UiContainer v-if="isLoading">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
-
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-else-if="errMsg">
+        <UiAlert>{{ errMsg }}</UiAlert>
       </UiContainer>
+      <MeetupView v-else :meetup="meetup"/>
     </div>`,
 });
